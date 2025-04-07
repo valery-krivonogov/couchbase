205226a5f1f27a0fb822f229fe7dd6b154712a38
valery-krivonogov <valeryii@mail.ru>
2025-04-07 08:55:09 +0300

    Create test_python.py

---
+++ test_python.py
@@ -0,0 +1,19 @@
+Пример подключения из приложения (python)
+Upsert CAS:
+CAS =1743242072126062592
+
+Get airline for key = airline_111222333 :
+Data:
+{'type': 'airline', 'id': 111222333, 'callsign': 'TEST', 'iata': 'SVO', 'icao': 'ULN', 'name': 'My Airways'}
+name: My Airways
+callsign: TEST
+iata: SVO
+icao: ULN
+
+Get airline for key = airline_10 :
+Data:
+{'id': 10, 'type': 'airline', 'name': '40-Mile Air', 'iata': 'Q5', 'icao': 'MLA', 'callsign': 'MILE-AIR', 'country': 'United States'}
+name: 40-Mile Air
+callsign: MILE-AIR
+iata: Q5
+icao: MLA
